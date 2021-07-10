1ef42e6fef3c77bca2b589e41d5710e2aed865e3
GreigBenton <greigbenton101@gmail.com>
2021-07-10 17:25:38 +0000

    added profile page template models and views

--- userprofiles/views.py
+++ userprofiles/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
+from .models import Profile
+from .forms import ProfileForm
 
 
 def profile(request):
     """ Display the user's profile. """
+    profile = get_object_or_404(Profile, user=request.user)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = ProfileForm(instance=profile)
+    orders = profile.orders.all
 
     template = 'userprofiles/profile.html'
-    context = {}
+    context = {
+        'form': form,
+        'orders': orders,
+    }
 
     return render(request, template, context)
