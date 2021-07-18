55f8776ff60dc7f22c19189e8cc510543ca90cd5
GreigBenton <greigbenton101@gmail.com>
2021-07-18 16:16:01 +0000

    completed product admin

--- userprofiles/views.py
+++ userprofiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import Profile
 from .forms import ProfileForm
@@ -7,6 +8,7 @@ from .forms import ProfileForm
 from checkout.models import Order
 
 
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(Profile, user=request.user)
